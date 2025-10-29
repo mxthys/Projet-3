05d7520f488260566bd0b65637d418a3019c2847
mathys <mathyscolomina38@gmail.com>
2025-10-29 20:49:39 +0100

    page de connexion fonctionnel

---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,51 @@
+//PARTIE 3 RECUPERER LES VALEURS ENTREES ET LES ENVOYER A L'API
+//Récuperer les valeurs entrées
+const loginForm = document.getElementById("login-Form");
+
+loginForm.addEventListener("submit", function(event) { 
+
+    event.preventDefault(); // empêche le rechargement de la page
+
+    const mail = document.getElementById("email").value
+    const userPW = document.getElementById("userPW").value
+
+    console.log(`Le mail est : ${mail} le mot de passe est ${userPW}`)
+
+    //Les mettre dans un objet 
+const identifiants = {
+    email : mail,
+    password : userPW
+}
+//Créer la charge utile
+const chargeUtile = JSON.stringify(identifiants)
+
+//envoyer les valeurs
+fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {"Content-Type"  : "application/json"},
+    body : chargeUtile
+})
+
+//Gérer la réponse de l'API
+.then(response => response.json())
+  .then(data => {
+    console.log("Réponse du serveur :", data);
+
+    if (data.token) {
+      console.log("✅ Connexion réussie !");
+      // Sauvegarder le token pour plus tard
+      localStorage.setItem("token", data.token);
+      // Rediriger vers la page principale
+      window.location.href = "index.html";
+    } else {
+      console.log("❌ Identifiants incorrects !");
+      alert("E-mail ou mot de passe incorrect.");
+    }
+  })
+  .catch(error => {
+    console.error("Erreur réseau :", error);
+  });
+});
+
+
+
